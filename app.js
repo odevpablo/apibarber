77cb74f2e7262a2b4f32f0f3140e5759a7ab233a
SrPabaum <mrmster6@gmail.com>
2024-01-04 23:02:40 -0300

    up

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
-const cors = require('cors'); 
+const fs = require('fs').promises;  
+const cors = require('cors');
 
 const app = express();
 const port = 3001;
@@ -9,26 +9,51 @@ const port = 3001;
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/api/salvarSelecao', (req, res) => {
+let horarios = ['07:00', '08:00', '09:00', '10:00'];
+const selecoesFilePath = 'selecoes.json';
+
+app.get('/api/horarios', (req, res) => {
+  res.json(horarios);
+});
+
+app.post('/api/salvarSelecao', async (req, res) => {
   try {
-    const { selectedCard, otherData } = req.body;
+    const { nome, selectedCard, otherData, horario } = req.body;
+
+    if (!nome || !selectedCard || !horario) {
+      return res.status(400).json({ error: 'Campos obrigatórios ausentes' });
+    }
 
     const data = {
+      nome,
       selectedCard,
       otherData,
+      horario,
     };
 
-    fs.writeFile('selecoes.json', JSON.stringify(data), (error) => {
-      if (error) {
-        console.error('Erro ao salvar seleção:', error);
-        res.status(500).json({ error: 'Erro interno do servidor ao salvar seleção' });
-      } else {
-        res.json({ message: 'Seleção salva com sucesso!' });
-      }
-    });
+    await fs.writeFile(selecoesFilePath, JSON.stringify(data));
+    console.log('Seleção salva com sucesso!');
+    res.json({ message: 'Seleção salva com sucesso!' });
   } catch (error) {
     console.error('Erro ao salvar seleção:', error);
     res.status(500).json({ error: 'Erro interno do servidor ao salvar seleção' });
+  }
+});
+
+app.post('/api/alterarHorarios', (req, res) => {
+  try {
+    const { novosHorarios } = req.body;
+
+    if (!novosHorarios || !Array.isArray(novosHorarios)) {
+      return res.status(400).json({ error: 'Dados inválidos para alteração de horários' });
+    }
+
+    horarios = novosHorarios;
+
+    res.json({ message: 'Horários alterados com sucesso!' });
+  } catch (error) {
+    console.error('Erro ao alterar horários:', error);
+    res.status(500).json({ error: 'Erro interno do servidor ao alterar horários' });
   }
 });
 
